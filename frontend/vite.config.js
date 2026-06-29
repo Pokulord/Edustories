25f5955c28a663c0e8cbab9020f39bb7932cd761
Svyat <svyatos.chern@mail.ru>
2026-06-29 15:38:52 +0500

    Changed config to deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/Edustories/',
+   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        cabinet: resolve(__dirname, 'cabinet.html'),
+        profile: resolve(__dirname, 'profile.html'),
+      },
+    },
+  },
 })
